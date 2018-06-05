64aa90f6a196f19127f3bc3b2033a2fc64bfd5fc
j2h4u <ccc911.ngs.ru@gmail.com>
2018-06-05 13:25:37 +0700

    new way of attaching NVRM & autofan

--- xinit.user.sh
+++ xinit.user.sh
@@ -4,5 +4,7 @@
 
 # uncomment the following line if you want to see miner log after start
 #tail -f /run/hive/miner.1
-screen -dmS NVRM /home/user/nvrm.sh
-screen -dmS autofan /home/user/autofan.sh
+#screen -dmS NVRM /home/user/nvrm.sh
+#screen -dmS autofan /home/user/autofan.sh
+screen -S agent -x -t "NVRM" -X screen bash -c '/home/user/nvrm.sh; exec bash'
+screen -S agent -x -t "autofan" -X screen bash -c '/home/user/autofan.sh; exec bash'
